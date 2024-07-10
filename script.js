3413d536d811ac6563cff89182cad34d843c32bd
celineMend <celine.mendy@unchk.edu.sn>
2024-07-10 13:37:22 +0100

    initialisation

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//     let submissionForm = document.getElementById('submissionForm');
+//     submissionForm.addEventListener('submit',function(e) {
+
+//     let myInput = document.getElementById('username');
+//     let myregex = (/a-ZA-z- s/+$);
+
+// if(myInput.ariaValueMax.trim()==""){
+
+//     let error = document.getElementById('error');
+//     error.innerHTML = " le champs usermane est requis"
+//     error.style.color = 'red';
+//     e.preventDefault();
+// }else if () {
+
+// }
+   
+
+// })
